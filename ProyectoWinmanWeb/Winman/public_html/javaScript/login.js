04e3653d32195fe741f7bdb729ed4db2586cedee
fcorcoll <francesc901@gmail.com>
2018-02-13 10:27:54 +0000

    Update login.js

--- ProyectoWinmanWeb/Winman/public_html/javaScript/login.js
+++ ProyectoWinmanWeb/Winman/public_html/javaScript/login.js
@@ -8,8 +8,10 @@ $('input[type="text"],input[type="password"]').css("border","2px solid red");
 $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
 alert("Por favor rellene todo los campos...!");
 }else {
-$.post("login.php",{ email1: email, password1:password},
+$.post("http://localhost/winmanWeb-master/ProyectoWinmanWeb/Winman/public_html/login.php",
+{ email1: email, password1:password},
 function(data) {
+	alert(data)
 if(data=='Invalid Email.......') {
 $('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
 $('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
