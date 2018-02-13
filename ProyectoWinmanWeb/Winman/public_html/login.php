234ba436c52d2d51027395ea17b04a29e2364cc5
fcorcoll <francesc901@gmail.com>
2018-02-13 10:26:52 +0000

    Update login.php
    working connection onBD winman.

--- ProyectoWinmanWeb/Winman/public_html/login.php
+++ ProyectoWinmanWeb/Winman/public_html/login.php
@@ -1,24 +1,26 @@
 
 
 <?php
-$connection = mysql_connect("localhost", "root", ""); // Establishing connection with server..
-$db = mysql_select_db("winman", $connection); // Selecting Database.
+$connection = mysqli_connect("localhost", "root", "mysql", "winman"); // Establishing connection with server..
 $email=$_POST['email1']; // Fetching Values from URL.
-$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+$password=($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+
 // check if e-mail address syntax is valid or not
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Invalid Email.......";
-}else{
+//$email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
+//if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+//echo "Invalid Email.......";
+//}else{
 // Matching user input email and password with stored email and password in database.
-$result = mysql_query("SELECT * FROM usuario WHERE email='$email' AND contrasenya='$password'");
-$data = mysql_num_rows($result);
+$result = mysqli_query($connection, "SELECT * FROM usuario WHERE email='$email' AND contrasenya='$password'");
+echo $email, $password;
+$data = mysqli_num_rows($result);
+
 if($data==1){
 echo "Successfully Logged in...";
 }else{
 echo "Email or Password is wrong...!!!!";
 }
-}
-mysql_close ($connection); // Connection Closed.
+//}
+mysqli_close ($connection); // Connection Closed.
 ?>
 
